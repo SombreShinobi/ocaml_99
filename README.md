c77c06cbeb002bc9aec6679197473f728203b171
SombreShinobi <sedlan.sibin@gmail.com>
2024-01-23 15:32:18 +0100

    feat: first three, readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ocaml_99
+
+Solving [99 Ocaml Problems](https://v2.ocaml.org/learn/tutorials/99problems.html) just for fun.
--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,26 @@
-let () = print_endline "Hello, World!"
+let rec last xs =
+    match xs with
+    | [] -> None
+    | [ x ] -> Some x
+    | _ :: rest -> last rest
+;;
+
+let rec last_two xs =
+    match xs with
+    | [] -> None
+    | [ x; y ] -> Some (x, y)
+    | _ :: rest -> last_two rest
+;;
+
+let rec nth (xs : 'a list) (n : int) =
+    match xs with
+    | x :: _ when n == 1 -> Some x
+    | _ :: rest when n > 1 -> nth rest (n - 1)
+    | _ -> None
+;;
+
+let () =
+    match nth [ "a"; "b"; "c"; "d"; "f"; "g" ] 1 with
+    | Some x -> print_endline x
+    | None -> print_endline "Ain't none"
+;;
